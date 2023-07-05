38ddea55a7f7d059f00374bcdac290313f7d5fba
Dmytro Kazantsev <dk@roboticket.com>
2023-07-05 20:10:32 +0200

    fix: deleted symbol

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,7 +3,7 @@ import style from './styles.module.css'
 export const About = () => {
     return (
         <section className={style.aboutWrapper}>
-            <p className={style.aboutItem}>Hi! I'm a software developer from Ukraine! 🚀</p>
+            <p className={style.aboutItem}>Hi! I&apos;m a software developer from Ukraine! 🚀</p>
             <p className={style.aboutItem}>I love to turn business ideas into reality with the help of code using modern technologies! 🔥</p>
         </section>
     )
